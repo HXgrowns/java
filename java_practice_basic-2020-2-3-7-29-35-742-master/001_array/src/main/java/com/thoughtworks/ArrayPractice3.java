56e6b3715a1095a06d704e3ffeb10eb21c9e108b
huxiao <804156017@qq.com>
2020-02-05 11:54:26 +0800

    [huxiao] {delete}: delete chinese code

--- java_practice_basic-2020-2-3-7-29-35-742-master/001_array/src/main/java/com/thoughtworks/ArrayPractice3.java
+++ java_practice_basic-2020-2-3-7-29-35-742-master/001_array/src/main/java/com/thoughtworks/ArrayPractice3.java
@@ -3,10 +3,6 @@ package com.thoughtworks;
 import java.util.ArrayList;
 
 public class ArrayPractice3 {
-
-    /**
-     * 完成本方法实现功能: 将一个整型数组array中值0的元素去掉，并把剩下的元素组成新的数组
-     */
     public static int[] filterZero(int[] array) {
         ArrayList<Integer> newArray = new ArrayList<Integer>();
 
